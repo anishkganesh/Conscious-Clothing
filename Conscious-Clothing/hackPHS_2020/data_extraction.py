0c7d31f43601ed49dabe9bcbed4ddaf824f46ce3
anishkganesh <35405014+anishkganesh@users.noreply.github.com>
2024-09-07 14:31:47 -0400

    Update data_extraction.py

--- Conscious-Clothing/hackPHS_2020/data_extraction.py
+++ Conscious-Clothing/hackPHS_2020/data_extraction.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+# Data Access from online clothing article page
 def data_parser(url):
     headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0'}
 
