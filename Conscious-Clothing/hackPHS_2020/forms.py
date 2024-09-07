a6bcf0b4091dcd84b248bb73605691960e5f0e51
anishkganesh <35405014+anishkganesh@users.noreply.github.com>
2024-09-07 14:33:01 -0400

    Update forms.py

--- Conscious-Clothing/hackPHS_2020/forms.py
+++ Conscious-Clothing/hackPHS_2020/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, TextAreaField
 # note: pip install wtforms[email] must be done separately if done on new system
 from wtforms.validators import DataRequired
 
-
+# Clothing Form
 class ClothingForm(FlaskForm):
     link = TextAreaField('Paste Link Here:', validators=[DataRequired()])
     submit = SubmitField('Submit')
